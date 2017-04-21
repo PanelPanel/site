381d3cb9ff1c07cf5664eb0537225cfae54a3fda
Auto Git <autogit@localhost>
2017-04-21 14:13:09 -0400

    Added a cahce buster for logomark.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,13 +5,15 @@ $myimage = new Asset('p/panel.png');
 //get first color of first visible exhibition
 $getColor = page('exhibitions')->children()->visible()->sortBy('sort', 'desc')->first()->bgcolor();
 
+$getRandom = str::quickRandom(5);
+
 ?>
 
   <header class="header-main" style="background-color:<?php echo $getColor; ?>;" role="banner">
     <div class="grid">
 
       <div class="branding column">
-        <a href="<?= url() ?>" rel="home"><?php echo thumb($myimage, array('width' => 600)); ?></a>
+        <a href="<?= url() ?>" rel="home"><img src="<?php echo thumb($myimage, array('width' => 600))->url() .'?'. $getRandom; ?>"/></a>
       </div>
 
       <?php snippet('menu') ?>
