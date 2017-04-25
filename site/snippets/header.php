cd8a770b7f834c5818c12707b3287232fac1c651
Auto Git <autogit@localhost>
2017-04-24 23:23:55 -0400

    LOGO. Fixed cache-ing problem.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,18 +1,19 @@
 <?php
-//get identity images
-$myimage = new Asset('p/panel.png');
 
 //get first color of first visible exhibition
 $getColor = page('exhibitions')->children()->visible()->sortBy('sort', 'desc')->first()->bgcolor();
 
-$getRandom = str::quickRandom(5);
+$getRandom = str::quickRandom(7);
+
+//get identity images
+$myimage = new Asset('p/panel.png?' . $getRandom );
 
 ?>
 
   <header class="header-main" style="background-color:<?php echo $getColor; ?>;" role="banner">
 
       <div class="branding column">
-        <a href="<?= url() ?>" rel="home"><img src="<?php echo thumb($myimage, array('width' => 600))->url() .'?'. $getRandom; ?>"/></a>
+        <a href="<?= url() ?>" rel="home"><img src="<?php echo $myimage->url() ?>"/></a>
       </div>
 
       <?php snippet('menu') ?>
